f0a3f904e67e48bd374976657e39eb9dc2993f24
Miguel Juncal Paz <migueljuncalpaz@icloud.com>
2026-08-12 13:53:07 +0200

    add tests

---
+++ src/del.js
@@ -0,0 +1,5 @@
+function del(a, b) {
+  return a - b;
+}
+
+module.exports = del;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,12 @@
 const sum = require('../src/sum');
+const del = require('../src/del');
+
 
 test('suma 1 + 2 y devuelve 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+
+test('resta 1 - 2 y devuelve -1', () => {
+  expect(del(1, 2)).toBe(-1);
 });
